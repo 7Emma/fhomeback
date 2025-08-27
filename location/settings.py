8f5e6cd29a530dbf32556fec7bcc585ead8de197
Emmanuel AGBOTOEDO <205164160+emmaagbo@users.noreply.github.com>
2025-08-27 04:34:42 +0100

    back

--- location/settings.py
+++ location/settings.py
@@ -8,14 +8,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # --- Sécurité ---
+
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())
-CORS_ALLOWED_ORIGINS = config(
-    "CORS_ALLOWED_ORIGINS",
-    default="http://localhost:5173",
-    cast=Csv()
-)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
+CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", cast=Csv())
+
 
 # --- Applications installées ---
 INSTALLED_APPS = [
